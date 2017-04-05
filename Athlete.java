3820613023e0b2f7e19422b43a51c30275c4a7c7
rmit-s3582660-pan-ze <s3582660@student.rmit.edu.au>
2017-04-05 19:00:57 +1000

    Add files via upload

--- Athlete.java
+++ Athlete.java
@@ -1,46 +1,56 @@
 import java.util.Random;
-
-public class Athlete extends Participant {
-
-	public Athlete(String ID, String name, int age, String state,String athleteType) {
-		super(ID, name, age, state);
-		// TODO Auto-generated constructor stub
-	}
-    private int current_score;
-    private String game_type; 
-	private int scores=0;
-	public int compete(String game)
-	{
-		Random r = new Random();
-		int time = 0;
-		if(game.equals("running"))
+											//ze pan s3582660
+public class Athlete extends Participants  //class athlete: to generate the grades for games
+ {
+	 private int scores;//total score of athletes
+	 private int current_score;//current game score 
+	 private String type;//
+	 
+	 public Athlete(String ID_, String name_, int age_, String state_, String type_)
+	 {
+		 super(ID_, name_, age_, state_);
+		 type = type_;
+		 scores = 0;
+		 current_score = 0;
+	 }
+	 
+	 public int compete(String game)//generate the grade of game by random
+	 {
+		 Random r = new Random();
+		 int value = 0;
+		 if(game.equals("running"))
 		 {
-			 time = r.nextInt(10) + 10;
+			 value = r.nextInt(10) + 10;
 		 }
 		 else if(game.equals("swimming"))
 		 {
-			 time = r.nextInt(100) + 100;
+			 value = r.nextInt(100) + 100;
 		 }
 		 else if(game.equals("cycling"))
 		 {
-			 time = r.nextInt(300) + 500;
+			 value = r.nextInt(300) + 500;
 		 }
-		 current_score = time;
-		 return time;
+		 current_score = value;
+		 return value;
 	 }
-	 public int get_current()
+	 
+	 public int get_current()//get score of current game  
 	 {
 		 return current_score;
 	 }
 	 
-	 public void set_scores(int score)
+	 public void set_scores(int score)//set the total score 
 	 {
 		 scores += score;
 	 }
 	 
-	 public void print()
+	 public String get_type()
 	 {
-		 System.out.print(get_ID() + "\t" + get_name() + "\t" + get_age() + "\t" + get_state() + "\t" + game_type + "\t" + scores + "\t");
+		 return type;
+	 }
+	 
+	 public void print()//output all the information of  athletes
+	 {
+		 System.out.print(get_ID() + "\t" + get_name() + "\t" + get_age() + "\t" + get_state() + "\t" + type + "\t" + scores + "\t");
 	 }
  }
-
